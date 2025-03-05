97a82235304d73dcb76c7213c88bdea2d8dd61b1
ds58 <30220598+ds58@users.noreply.github.com>
2025-03-05 11:46:41 -0600

    :arrow_up: ihmc-native-library-loader 2.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,6 @@ ihmc {
 
 mainDependencies {
    api("us.ihmc:javacpp:1.5.11-ihmc-2")
-   api("us.ihmc:ihmc-native-library-loader:2.0.3")
+   api("us.ihmc:ihmc-native-library-loader:2.0.4")
    api("us.ihmc:log-tools:0.6.3")
 }
